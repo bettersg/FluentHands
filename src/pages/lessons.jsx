ce95ff4bbff1ce38be0984bb2e6e7d67b874be78
Fabian suwibowo <elyovsun@gmail.com>
2024-12-29 17:05:00 +0800

    clean up code

--- src/pages/lessons.jsx
+++ src/pages/lessons.jsx
@@ -38,33 +38,6 @@ export default function Lessons() {
   const row5 = [20, 21, 22, 23, 24];
   const row6 = [25];
 
-  // const [doneA, setDoneA] = useState(false);
-  // const [doneB, setDoneB] = useState(false);
-  // const [doneC, setDoneC] = useState(false);
-  // const [doneD, setDoneD] = useState(false);
-  // const [doneE, setDoneE] = useState(false);
-  // const [doneF, setDoneF] = useState(false);
-  // const [doneG, setDoneG] = useState(false);
-  // const [doneH, setDoneH] = useState(false);
-  // const [doneI, setDoneI] = useState(false);
-  // const [doneJ, setDoneJ] = useState(false);
-  // const [doneK, setDoneK] = useState(false);
-  // const [doneL, setDoneL] = useState(false);
-  // const [doneM, setDoneM] = useState(false);
-  // const [doneN, setDoneN] = useState(false);
-  // const [doneO, setDoneO] = useState(false);
-  // const [doneP, setDoneP] = useState(false);
-  // const [doneQ, setDoneQ] = useState(false);
-  // const [doneR, setDoneR] = useState(false);
-  // const [doneS, setDoneS] = useState(false);
-  // const [doneT, setDoneT] = useState(false);
-  // const [doneU, setDoneU] = useState(false);
-  // const [doneV, setDoneV] = useState(false);
-  // const [doneW, setDoneW] = useState(false);
-  // const [doneX, setDoneX] = useState(false);
-  // const [doneY, setDoneY] = useState(false);
-  // const [doneZ, setDoneZ] = useState(false);
-
   function toggleDone(id) {
     setDone((doneState) =>
       doneState.map((done) =>
@@ -73,46 +46,6 @@ export default function Lessons() {
           : done
       ))
   }
-
-  // function toggleDoneA() {
-  //   setDoneA((doneState => !doneState));
-  // }
-
-  // function toggleDoneB() {
-  //   setDoneB((doneState => !doneState));
-  // }
-
-  // function toggleDoneC() {
-  //   setDoneC((doneState => !doneState));
-  // }
-
-  // function toggleDoneD() {
-  //   setDoneD((doneState => !doneState));
-  // }
-
-  // function toggleDoneE() {
-  //   setDoneE((doneState => !doneState));
-  // }
-
-  // function toggleDoneF() {
-  //   setDoneF((doneState => !doneState));
-  // }
-
-  // function toggleDoneG() {
-  //   setDoneG((doneState => !doneState));
-  // }
-
-  // function toggleDoneH() {
-  //   setDoneH((doneState => !doneState));
-  // }
-
-  // function toggleDoneI() {
-  //   setDoneI((doneState => !doneState));
-  // }
-
-  // function toggleDoneJ() {
-  //   setDoneJ((doneState => !doneState));
-  // }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', marginTop: '50px' }}>
